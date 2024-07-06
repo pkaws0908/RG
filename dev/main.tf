2df457c6479260f82e9d8a83ed5e86e06935a3dd
Prashant Kumar <92239848+pkaws0908@users.noreply.github.com>
2024-07-07 00:48:16 +0530

    Update main.tf

--- dev/main.tf
+++ dev/main.tf
@@ -2,5 +2,8 @@ resource "azurerm_resource_group" "rg" {
   name     = "devrg0018581"
   location = "West Europe"
 }
-
+resource "azurerm_resource_group" "rg1" {
+  name     = "devrg00185811"
+  location = "West Europe"
+}
 
