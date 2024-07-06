9c8a1d48198a237af643dd241119d41d0b603934
Prashant Kumar <92239848+pkaws0908@users.noreply.github.com>
2024-07-06 23:45:25 +0530

    Update main.tf

--- dev/main.tf
+++ dev/main.tf
@@ -3,21 +3,4 @@ resource "azurerm_resource_group" "rg" {
   location = "West Europe"
 }
 
-resource "azurerm_storage_account" "sa" {
-  name                     = "devsa0018581"
-  resource_group_name      = azurerm_resource_group.rg.name
-  location                 = azurerm_resource_group.rg.location
-  account_tier             = "Standard"
-  account_replication_type = "LRS"
-
-  tags = {
-    environment = "qa"
-  }
-}
-
-resource "azurerm_storage_container" "ca1" {
-  name                  = "devcont00858"
-  storage_account_name  = azurerm_storage_account.sa.name
-  container_access_type = "private"
-}
 
